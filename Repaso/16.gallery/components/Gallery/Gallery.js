0bc13223a0aed5801973c5cc6eb241e9b36ce53a
Antonio Rosales Martínez <antoniorosalesmartinez@MacBook-Pro-de-Antonio.local>
2023-09-13 11:23:23 +0200

    feat gallery

--- Repaso/16.gallery/components/Gallery/Gallery.js
+++ Repaso/16.gallery/components/Gallery/Gallery.js
@@ -1,42 +1,71 @@
 import "./Gallery.css";
 
+let keyword = "";
+let perPage = "";
+let pageNum = 1;
+
 const template = () => {
   return `
   <div class="searchField">
     <input type="text" id="searchInput" />
+    <select id="pageSelect">
+      <option value="10">10</option>
+      <option value="20">20</option>
+      <option value="30">30</option>
+    </select>
     <button id="searchBtn">Search</button>
+    <button id="prevBtn">◀</button>
+    <button id="nextBtn">▶</button> 
   </div>
   <ul id="result">
   </ul>
   `;
 };
 
-const getPhotos = async (keyword) => {
+const getPhotos = async (keyword, numPhotos, pageNum) => {
   const data = await fetch(
-    `https://api.unsplash.com/search/photos?client_id=674SfpAIQXpkf5a3XT96nJDFugHWHVpc3-q5o_A_nNk&query=${keyword}&per_page=20`
+    `https://api.unsplash.com/search/photos?client_id=${
+      import.meta.env.VITE_UNSPLASH_KEY
+    }&query=${keyword}&per_page=${numPhotos}&page=${pageNum}`
   );
   const dataJSON = await data.json();
-  printPhotos(dataJSON.results)
+  console.log(dataJSON);
+  printPhotos(dataJSON.results);
 };
 
 const printPhotos = (list) => {
   const ul = document.querySelector("#result");
-  ul.innerHTML = ""
+  ul.innerHTML = "";
   for (const photo of list) {
     const li = document.createElement("li");
     li.innerHTML = `
-    <img src=${photo.urls.small} alt=${photo.alt_description} />
-    `
-    ul.appendChild(li)
+    <img src=${photo.urls.small} alt=${photo.alt_description} style="border: 10px solid ${photo.color}" />
+    `;
+    ul.appendChild(li);
   }
-}
+};
 
 const Gallery = () => {
   document.querySelector("main").innerHTML = template();
+  //Search
   document.querySelector("#searchBtn").addEventListener("click", () => {
     const searchInput = document.querySelector("#searchInput");
-    getPhotos(searchInput.value)
-  })
+    const pageSelect = document.querySelector("#pageSelect");
+    keyword = searchInput.value;
+    perPage = pageSelect.value;
+    pageNum = 1;
+    getPhotos(searchInput.value, pageSelect.value, 1);
+  });
+  //Next
+  document.querySelector("#nextBtn").addEventListener("click", () => {
+    pageNum++;
+    getPhotos(keyword, perPage, pageNum);
+  });
+  //Prev
+  document.querySelector("#prevBtn").addEventListener("click", () => {
+    pageNum--;
+    getPhotos(keyword, perPage, pageNum);
+  });
 };
 
 export default Gallery;
